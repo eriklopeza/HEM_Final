7c4f7badee47dcbde4084c5c0bc8311e8a7c16bc
Mandeep Masoun <56169771+Trusty22@users.noreply.github.com>
2024-03-06 20:44:10 -0800

    Update svc.s

--- HemFinal/svc.s
+++ HemFinal/svc.s
@@ -15,7 +15,28 @@ SYS_FREE		EQU		0x5		; address 20007B14
 ; System Call Table Initialization
 		EXPORT	_syscall_table_init
 _syscall_table_init
-	;; Implement by yourself
+	; AFTER ADRESS FOUND send it to its corosponding k func 
+	;  FIGUREOUT ACTION AND 
+	; compare if 0 is first run through 
+	; if not then find action and send to its k function
+	; if equal than store there in this case in sysmalloc
+  LDR     r0, =SYSTEMCALLTBL   ; Load the base address of the system call table
+
+    LDR     r1, =SYS_EXIT         ; Load the address of SYS_EXIT
+    STR     r1, [r0, #0]          ; Store SYS_EXIT address at index 0 in the table
+    LDR     r1, =SYS_ALARM        ; Load the address of SYS_ALARM
+    STR     r1, [r0, #4]          ; Store SYS_ALARM address at index 4 in the table
+    LDR     r1, =SYS_SIGNAL       ; Load the address of SYS_SIGNAL
+    STR     r1, [r0, #8]          ; Store SYS_SIGNAL address at index 8 in the table
+    LDR     r1, =SYS_MEMCPY       ; Load the address of SYS_MEMCPY
+    STR     r1, [r0, #12]         ; Store SYS_MEMCPY address at index 12 in the table
+    LDR     r1, =SYS_MALLOC       ; Load the address of SYS_MALLOC
+    STR     r1, [r0, #16]         ; Store SYS_MALLOC address at index 16 in the table
+    LDR     r1, =SYS_FREE         ; Load the address of SYS_FREE
+    STR     r1, [r0, #20]         ; Store SYS_FREE address at index 20 in the table
+    MOV     pc, lr                ; Return from the subroutine
+           
+	
 	
 		MOV		pc, lr
 
@@ -25,7 +46,26 @@ _syscall_table_init
 _syscall_table_jump
 	;; Implement by yourself
 	
-		MOV		pc, lr			
+	
+	LDR     R4, =SYSTEMCALLTBL   ; Load the base address of the system call table
+	LDR		R5, [R4]
+	CMP 	R5, R7 ; r7 adress to r4 value
+	BEQ done
+loop	
+	ADD		R4, #4
+	LDR		R5, [R4]
+	CMP 	R5, R7	
+	BEQ		done
+	B loop
+	
+	
+done
+	 ; call kalloc in heap tp allocate memory
+	 ; implement kalloc table
+	BX LR
+	
+	; BL      _syscall_table_init                    ; Branch to the system call function
+	 MOV		pc, lr			
 		
 		END
 
