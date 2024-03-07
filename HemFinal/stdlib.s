d51b3b29a8c1a3862fc3acb4f8ada4deec00ad99
Mandeep Masoun <56169771+Trusty22@users.noreply.github.com>
2024-03-06 20:42:22 -0800

    Update stdlib.s

--- HemFinal/stdlib.s
+++ HemFinal/stdlib.s
@@ -15,7 +15,7 @@ _bzero
 
 loop    
 		CMP     r1, #0          ; Check if n is zero
-        BEQ     bz_done            ; If so, exit the loop
+        BEQ     bz_done            ; If so, exit the loop  
 
         STRB    r2, [r0], #1    ; Store zero byte at the memory location pointed by r0, and increment r0
         SUBS    r1, r1, #1      ; Decrement n
@@ -34,10 +34,11 @@ bz_done
 ; Return value
 ;   dest 
 
-	EXPORT	_strncpy
+		EXPORT	_strncpy
 _strncpy
 		
 		MOV R5, R2 ; r2 = 40, r1 = a r0 = b
+		
 	
 cpy_loop
 		LDRB R4, [R1], #1
@@ -49,14 +50,16 @@ cpy_loop
 
 cpy_done
 	;MOV R2,R1
+	
 	MOV		PC, LR
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; void* _malloc( int size )
 ; Parameters
 ;	size	- #bytes to allocate
 ; Return value
 ;   	void*	a pointer to the allocated space
-		EXPORT	_malloc
+			EXPORT	_malloc
 _malloc
 		; save registers
 		STMDB sp!, {r1-r12, lr}	; save all registers that could be changed
@@ -64,10 +67,13 @@ _malloc
 		; r0 = size
 		
 		; set the system call # to R7
+	;	MOV R1, 
 		MOV	r7, #0x4
-	        SVC     #0x0
+	        SVC     #0x4
+		MOV R0,R0
+		;STRB     R4, [R4, R0] ; allocate bytres
 		; resume registers
-		MOV		r0, r4
+		
 		LDMIA sp!, {r1-r12, lr} ; load back registers and return address
 
 		MOV		pc, lr
@@ -81,9 +87,14 @@ _malloc
 		EXPORT	_free
 _free
 		; save registers
+		STMDB sp!, {r1-r12, lr}	; save all registers that could be changed
+
 		; set the system call # to R7
-        	SVC     #0x0
+		MOV	r7, #0x5
+        	SVC     #0x5
+		
 		; resume registers
+		LDMIA sp!, {r1-r12, lr} ; load back registers and return address
 		MOV		pc, lr
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -97,9 +108,13 @@ _free
 		EXPORT	_alarm
 _alarm
 		; save registers
+		STMDB sp!, {r1-r12, lr}	; save all registers that could be changed
 		; set the system call # to R7
+		MOV	r7, #0x1
         	SVC     #0x0
 		; resume registers	
+		LDMIA sp!, {r1-r12, lr} ; load back registers and return address
+
 		MOV		pc, lr		
 			
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -113,9 +128,14 @@ _alarm
 		EXPORT	_signal
 _signal
 		; save registers
+		STMDB sp!, {r1-r12, lr}	; save all registers that could be changed
 		; set the system call # to R7
+		MOV	r7, #0x2
         	SVC     #0x0
 		; resume registers
+		LDMIA sp!, {r1-r12, lr} ; load back registers and return address
+
+		; not sure how to return properly
 		MOV		pc, lr	
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
