2fc5b2b612d7bee80089f4ab61fd197e5eed217a
Mandeep Masoun <56169771+Trusty22@users.noreply.github.com>
2024-03-09 20:56:38 -0800

    Update timer.s

--- HemFinal/timer.s
+++ HemFinal/timer.s
@@ -25,11 +25,11 @@ _timer_init
 	;; Implement by yourself
 	LDR		R1, =STCTRL				
 	LDR		R0, =STCTRL_STOP		
-		STR		R0, [R1]				
+		STR		R0, [R1]	; systick stops			
 	    
-    LDR     R0, =STRELOAD_MX   ; Load maximum reload value
-	LDR		r1, =STRELOAD			
-		STR     R0, [R1]        ; Write to SysTick control register
+    LDR     R0, =STRELOAD_MX   ; the time count is 1 sec
+	LDR		r1, =STRELOAD		; systick reload address	
+		STR     R0, [R1]        ; the value of STRELOAD also the max
 	
 		MOV		pc, lr		; return to Reset_Handler
 
@@ -41,19 +41,18 @@ _timer_start
 	;; Implement by yourself
 	LDR R1, =SECOND_LEFT	
 	LDR R7, [R1]		
-		STR R0, [R1]	
-		
+		STR R0, [R1]		; Store the value of the timer parameter (R0) into SECOND_LEFT memory location
   	
 	LDR R2, =STCTRL		
-	LDR R3, =STCTRL_GO		
+	LDR R3, =STCTRL_GO		;control value for timer start into register R3
 		STR R3, [R2]		
 	
   	
 	LDR R4, =STCURRENT
 	MOV R5, #0x0			
-		STR R5, [R4]	
+		STR R5, [R4]		; Store the value of 0x0 into STCURRENT register to reset the timer counter
+
 		
-  
 	MOV R0, R7	
 	MOV		pc, lr		; return to SVC_Handler
 
@@ -63,26 +62,28 @@ _timer_start
 		EXPORT		_timer_update
 _timer_update
 	;; Implement by yourself
-	
 
 	LDR		R1, =SECOND_LEFT	
-	LDR		R2, [R1] 		
+	LDR		R2, [R1] 		    ; Load the value stored in SECOND_LEFT memory location into register R2
 	SUB 	R2, R2, #1		
-	STR 	R2, [R1]
+	STR 	R2, [R1]     ; Store the decremented value back into SECOND_LEFT memory location to decrease the time
 	
-	
+	; if we dont have no more seconds left then we are done
 	CMP 	R2, #0
 	BNE		done_u
 	
 	LDR		R3, =STCTRL
 	LDR		R4, =STCTRL_STOP
-	STR		R4, [R3]
+	STR		R4, [R3]     ; Store the value of STCTRL_STOP into the STCTRL register to stop the timer
 	
-	LDR 	R5, =USR_HANDLER
-	LDR		R7, [R5]
-	
-	STMFD	sp!, {r1-r12,lr}		
+	LDR 	R5, =USR_HANDLER 
+	LDR		R7, [R5]     ; Load the value stored in USR_HANDLER memory location into register R7
+
+	; save all registers
+	STMFD	sp!, {r1-r12,lr}	
+	; this should be sig handler	
 	BLX 	R7
+	; load all registers
 	LDMFD	sp!, {r1-r12,lr}		
 
 done_u
@@ -95,10 +96,11 @@ done_u
 _signal_handler
 	;; Implement by yourself
 		CMP	R0, #SIGALRM
-		BNE	sig_done
+		BNE	sig_done	;if the alarm is not at the given val then we are done
 		LDR	R3, =USR_HANDLER
-		LDR	R2, [R3]
-		STR	R1, [R3]
+		LDR	R2, [R3] 		; Load the value stored in USR_HANDLER memory location into register R2	
+		STR	R1, [R3]			; Store the value of R1 into the USR_HANDLER memory location
+
 	
 sig_done
 		MOV	R0, R2
