08509f24ae16318c7391c475dc7b26b1de69b815
Mandeep Masoun <56169771+Trusty22@users.noreply.github.com>
2024-03-09 19:53:58 -0800

    Update heap.s

--- HemFinal/heap.s
+++ HemFinal/heap.s
@@ -19,29 +19,41 @@ INVALID		EQU		-1			; an invalid id
 ; Memory Control Block Initialization
 		EXPORT	_heap_init
 _heap_init
-	;; Implement by yourself
+
 
  ; Initialize (MCB)
-    LDR     r0, =MCB_TOP       ; Load address of MCB top
-    LDR     r1, =MAX_SIZE       ; Load address of MCB size
+    ; Load address of MCB top into register r0
+    LDR     r0, =MCB_TOP       
+	; Load address of MAX_SIZE into register r1
+    LDR     r1, =MAX_SIZE   
+    ; Store the value of MAX_SIZE into the memory location pointed to by MCB_TOP    
 	STR		R1, [R0]
-    MOV     r2, #0             ; Clear MCB entries
+	; Clear the MCB entries by Initialize register r2 to 0
+	MOV     r2, #0             
 	
 	LDR 	R0, =MCB_TOP
+	; Add 4 bytes to the address in R0 ( MCB entries are 4 bytes each)
 	ADD		R0, #4		; 0x20006804
+	; Load the base address of the maximum address range into register R1
 	LDR		R1, =0x20006C00 ;0x20006C00
 
+	; Compare the current address (R0) with the maximum address range (R1)
+	; & If the current address is at or beyond the maximum address range, branch to fin
 	CMP 	R0, R1 ; at  max?
 	BGE		fin
 	
-	STR		R2, [R0]			; clear mcb
+	; Inside the loop to clear MCB entries
+	; & Store the value in register R2 (which is 0) into the memory location pointed to by the current address (R0)
+	; Increment the address stored in R0 by 1 so we can make more space to store 
+	; 0 for more space, we do it twice for 
+	STR		R2, [R0]			
 	ADD		R0, R0, #1			
 	
 	STR		R2, [R0]
-	
-	ADD		R0, R0, #2		; A
+	; Increment the address stored in R0 by 2
+	ADD		R0, R0, #2		
 	B 		fin
-	
+; finish or end of init	
 fin
 		MOV		pc, lr
 
@@ -50,101 +62,130 @@ fin
 ; void* _k_alloc( int size )
 		EXPORT	_kalloc
 _kalloc
-	;; Implement by yourself
+		;Store registers
 		PUSH	{lr}
-		CMP	R0, #32 ; if the 32 bit limit is >=
+		; if the R0 bit limit is >= 32 then call pre_post_r to do the
+		; pre and post ralloc sequence
+		CMP	R0, #32 
 		BGE	pre_post_r
 		
 		MOV	R0, #32
 
 pre_post_r
-									
-		LDR	R1, =MCB_TOP			; R1 == MCB_TOP == left
-		LDR	R2, =MCB_BOT			; R2 == MCB_BOT == right
+		;The left and right adresses are stored respectivly  							
+		LDR	R1, =MCB_TOP			
+		LDR	R2, =MCB_BOT			
 		
 		BL	_ralloc
-		 
+		
+		;restore the lr and put the heap register we chose as the adress
+		; to return
 		POP		{lr}
 		MOV		R0, R12
 		
 		MOV		pc, lr
 		
 _ralloc		
-			; save reg
-			PUSH	{lr}
+			; save the lr 
+			PUSH {lr}
 			
+			; In the tabbed section we systematicly stored the parts of mcb that are needed
+			; the entire adress of the space of the MCB in r3
 			SUB	R3, R2, R1			
-			ADD	R3,  #MCB_ENT_SZ			; R3 == entire
+			ADD	R3,  #MCB_ENT_SZ			
 			
-			ASR	R4, R3, #1			; R4 == half
-			ADD	R5, R1, R4			; R5 == midpoint
+			;half adress of the whole of the MCB in r4 right and r5 is left
+			ASR	R4, R3, #1			
+			ADD	R5, R1, R4			
+			; the whole size of the act is found and put in r6 while its half is put in r7
+			LSL	R6, R3, #4			
+			LSL	R7, R4, #4			
 			
-			LSL	R6, R3, #4			; R6 == act_entire_size
-			LSL	R7, R4, #4			; R7 == act_half_size
-			
-			MOV	R12, #0			; R12 == heap_addr
+			; R12 was deemed to be our heap location so we cleared its space for memory
+			MOV	R12, #0			
 		
-		; do we need mem aloc?
+		; If we do have space small enough to enter memory then enter it
 		CMP	R0, R7
-		BGT	enter_mem ; dont need to allocate so we just enter it
+		; else shift the memory right and find avalible space to enter the memory in 
+		BGT	enter_mem 
 		
-		PUSH	{r0-r7}		; save registers
+		; we save registers for later
+		PUSH	{r0-r7}		
 		
+		; subtract the mcb whole size from the right side of the mcb and make it the new bottom
 		SUB	R2, R5, #MCB_ENT_SZ
 		BL	_ralloc
 		
-		POP		{r0-r7}		; resume registers
-		
+		; we restore the registers and finish
+		POP		{r0-r7}				
+
+		; if heap is null then shift over
 		CMP	R12, #0x0
 		BEQ	shift_heap
 		
-		LDR	R8, [R5]			; R8 == mem[midpoint]
+		; the value of the midpoint of the memory is put in r8 and do a bitwise operation of  0x01
+		LDR	R8, [R5]			
 		AND R8, #0x01
 		
+		;if r8 is empty then get the heap address and return it
 		CMP	R8, #0
 		BEQ	done_addr
 		B	r_done
 		
 shift_heap
-		PUSH	{r0-r7}		; save registers
+		; we save registers for later
+		PUSH	{r0-r7}		
+		; shift the heap top right so we can call ralloc for recursion 
+		; this helps later by finding a space that is avalible inside heap
 		MOV	R1, R5
 		BL	_ralloc
-		POP		{r0-r7}		; resume registers
+		; we restore the registers and finish
+		POP		{r0-r7}		
 		B 	r_done
 		
 		;done ad
 enter_mem
-		LDR 	R8, [R1]			; R8 == mem[left]
-		AND 	R8,  #0x1 
-		CMP		R8, #0 
-		BNE		negi			; return invalid
+		; This method Loads and checks if memory block is free
+		; then checks if memory block is within bounds
+		; then sets memory block status and finally update heap pointers
 		
-		LDR		R8, [R1]			; R8 == mem[left]
-		CMP		R8, R6
+		LDR 	R8, [R1]			; R8 has the left value of the memory 
+		AND 	R8,  #0x1 			; Then perform a bitwise operation with 0x1 to find the least significant bit
+									; checking if the memory block is in use
+		CMP		R8, #0 ; if its still empty then we  have an invalid case 
+		BNE		negi			
+		
+		LDR		R8, [R1]			; R8 has the left value of the memory
+		CMP		R8, R6				; and if it is the the whole size of act then invalid
 		BLT		negi			; return invalid
+								
+		ORR		R8, R6, #0x1	;Set status flag for memory block
+		STR		R8, [R1]		; Perform a bitwise OR operation of R6 and 0x1
+								; This operation sets the  flag or status bit in a memory block header. 
+								; spesificly array(m2a(left)) = act size
+								
+		LDR		R8, =MCB_TOP			
+		SUB		R1, R8			; the left side gets - from mcb top and multiplied by 16
+		LSL		R1, R1, #4			
 		
-		ORR		R8, R6, #0x1			; *(short *)&array[ m2a( left ) ] = act_entire_size | ; this chanmged 0x01;
-		STR		R8, [R1]
+		LDR		R10, =HEAP_TOP			
+		ADD		R10, R1			; the heap top gets added to the left side.
 		
-		
-		
-		LDR		R8, =MCB_TOP			; R8 == MCB_TOP
-		SUB		R1, R8			; left -= mcb_top
-		LSL		R1, R1, #4			; left *= 16
-		
-		LDR		R10, =HEAP_TOP			; R10 == HEAP_TOP
-		ADD		R10, R1			; heap_top += left
-		
-		MOV		R12, R10			; heap_addr = heap_top + ( left - mcb_top ) * 16
+		MOV		R12, R10			; This way we have the top of the heap address be the top of the heap plus the left address minus the mcb top time 16
+									; in order to have the acurate adress for the top of the new heap
 		B		r_done
 			
 done_addr
+		;if r8 is empty then get the heap address and return it
+		; otherwise this happens regardless
 		STR	R7, [R5]
 		B	r_done
 		
 negi
+		; if we have a negitive value its invalid so we clear heap 
 		MOV	R12, #0
-r_done
+r_done 
+		; return
 		POP		{lr}
 		BX		LR
 		
@@ -156,93 +197,118 @@ r_done
 		EXPORT	_kfree
 _kfree 
 		;; Implement by yourself
+			;save register 
 			PUSH	{lr}
-			LDR		R1, =HEAP_TOP				; Load the HEAP_TOP value into R2
-			LDR		R2, =HEAP_BOT				; Load the HEAP_BOT value into R3	
-			LDR  	R4, =MCB_TOP     			; Load the top of the MCB
-		
-		MOV		R3, R0					; Move pointer address into register R1
+			
+			;stored addresses in registers
+			LDR		R1, =HEAP_TOP				
+			LDR		R2, =HEAP_BOT					
+			LDR  	R4, =MCB_TOP     			
+	
+		; Pointer copy for future referance
+		MOV		R3, R0			
 
-		; If statement
+		; If the adress of top is out of boundes for either the heap top
 	    CMP  	R3, R1					; If address is smaller than HEAP_TOP
-    	BLT  	nullA			; Return Null
-   	 	
-		CMP  	R3, R2					; If address is larger than HEAP_BOT
-    	BGT  	nullA			; Return Null
+    	BLT  	nullA			
+   	 	; or the heap bottom return null
+		CMP  	R3, R2					
+    	BGT  	nullA			
 
-		 ; Compute the MCB address
-
+		; To find the MCB address we subract the heap top to the pointer
 		
     	SUB  	R5, R3, R1      			; Subtract HEAP_TOP from the pointer
-    	ASR  	R5, #4       			; Divide the difference by 16 (logical shift right by 4 bits)
-    	ADD  	R5, R4        			; Add the result to MCB_TOP to get the MCB address
+    	; Divide it by 4 bits  
+		ASR  	R5, #4      
+		; add the mcb top to get the address
+    	ADD  	R5, R4        			
 
-		; Call the _rfree function 
+		; Mcb becomes new pointer
 		MOV		R0, R5
 		
+		;save registers
 		PUSH 	{R1-R12}
-		
+		; call rfree with the new pointer
 		BL   	_rfree 
+		;resume registers
 		POP		{R1-R12}
-		
-		CMP  	R0, #0           			; Check if MCB address passed into _rfree() returns 0	
+		; If the mcb address is null then we have an error return null
+		CMP  	R0, #0           			
 		BEQ		nullA
 		POP		{LR}
 		MOV		pc, lr
 	
 _rfree	
+			;save registers
 			PUSH	{lr}
-											; R0 = MCB_addr
-			LDR R1, =MCB_TOP				; R1 = MCB_TOP
-			SUB R2, R0, R1		 		; R2 = mcb_offset -> mcb_addr - mcb_top
-			LDR R3, [R0]                 ; R3 = mcb_contents
-			ASR R4, R3, #4		 		; R4 = mcb_chunk
-			LSL R5, R3, #4		 		; R5 = my_size
-			LDR R7, =MCB_BOT
+			
+			; registers are loaded for future used adresses
+			LDR R1, =MCB_TOP				; mcb top
+			SUB R2, R0, R1		 		; mcb offfset
+			LDR R3, [R0]                 ; mcb value
+			ASR R4, R3, #4		 		; cmcb hunk
+			LSL R5, R3, #4		 		; size
+			LDR R7, =MCB_BOT			; mcb bottom for later	
+		STR R3, [R0]	
+		    ; Calculate buddy index
+  		SDIV R6, R2, R4      
+    	AND R6, #1  ; Check if buddy index is odd
 		
-		STR R3, [R0]
-  		SDIV R6, R2, R4
-    	AND R6, #1
-		
-      	CMP R6, #0			 		; Line 146 in heap.c
+      	CMP R6, #0	; Compare if buddy index is not equal to 0		
 		BNE odd		
-
+		
+		; if we haved an EVEN buddy index caluclaute for that
+		; Calculate address of buddy MCB
 		ADD R6, R0, R4
-  		
-  		CMP R6, R7					; Line 150 in heap.c
+		; Compare if address of buddy MCB is greater than or equal to MCB_BOT if it is = or greater then, program done and zero out
+  		CMP R6, R7					
 		BGE doneZ
 		
-    	LDR R7, [R6]				; R7 = mcb_buddy				  		
-    	AND R8, R7, #1				; Line 158 
+		; Load buddy MCB value into R7
+    	LDR R7, [R6]	
+		; Extract status flag from buddy MCB value
+    	AND R8, R7, #1	
+		; Compare if status flag is not equal to 0 if it's not = then program is done
       	CMP R8, #0
 		BNE done
-  
+		
+		; Calculate new size for merged memory blocks
   		ASR R7, #5
-    	LSL R7, #5				; Line 162
+    	LSL R7, #5		
+		; Compare if new size is not equal to current size if not then we are done
 		CMP R7, R5
-  		BNE done				; Line 163
+  		BNE done				
 
+		; Set status flag of buddy MCB to 0
 		MOV R8, #0
-      	STR R8, [R6]				; Line 168
+      	STR R8, [R6]				
+		; Double the size of memory block
 		LSL R5, #1				; my_size *= 2
   		STR R5, [R0]
-		BL _rfree					; Recursion (line 178)
+		; Recur here until we reach the end of the size
+		BL _rfree					
 		B done
 	
-odd								; Line 183
-     	SUB R6, R0, R4				; R6 = mcb_addr - mcb_chunk
+odd				
+		; ODD is very similar to even
+		; find the mcb address after minusing to the mcb chunk
+     	SUB R6, R0, R4			
+        ;Compare if MCB_TOP is greater than the calculated addressif it is  zero out and finish
        	CMP R1, R6
 	 	BGT doneZ
 		
-     	LDR R7, [R6]		; R7 = mcb_buddy 	
-		AND R8, R7,#1				; Line 195
+	    ; Load buddy MCB value into R7
+     	LDR R7, [R6]	
+		; Extract status flag from buddy MCB value and Compare if status flag is not equal to 0. If it is then we are done
+		AND R8, R7,#1				
   		CMP R8, #0
     	BNE done
-
+		
+	    ; Calculate new size for merged memory blocks
   		ASR R7,#5
-    	LSL R7,#5				; Line 199
+    	LSL R7,#5				;If the
       	CMP R7, R5
-		BNE done				; Line 200
+		BNE done				
 		
 		MOV R8,#0 
 		STR R8, [R0]
@@ -250,7 +316,7 @@ odd								; Line 183
 		STR R5, [R6]				; Line 207
 
 		MOV		R0, R6 
-		BL		_rfree					; Recursion (line 216)
+		BL		_rfree					; Recursion 
 		B		done
 		
 doneZ
